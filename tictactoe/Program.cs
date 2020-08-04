15b139f49b6dc41b32e569cee2cd6ae3b42f046d
Arseniy Arkadskov <ars.ark111@yandex.ru>
2020-08-04 17:38:01 +0300

    Repository initialised!

---
+++ tictactoe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tictactoe
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            int[,] board = new int[2, 2];
+            for (int x = 0; x < 3; x++)
+            {
+                
+            }
+        }
+    }
+}
